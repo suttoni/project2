82598283e89212425a820d32d0dfb5ba24f5a781
Ian <iss13@my.fsu.edu>
2015-11-05 18:28:44 -0500

    Update user_test.c

--- user_test.c
+++ user_test.c
@@ -6,7 +6,7 @@ int main(){
 
     /* 
        All three syscall's corresponding #'s can be found in syscall_64.tbl
-       Begin by requesting shuttle with syscall # of 324
+       Begin by requesting elevator with syscall # of 324
        0 refers to an adult (no longer using char, but int instead),
        1 refers to start_floor (current floor), while 3 refers to destination floor
     */
