effb11cdca4d9d380c6e5bf0909920f24ec6dcca
Ian <iss13@my.fsu.edu>
2015-11-06 18:03:46 -0500

    Update module_data.c

--- module_data.c
+++ module_data.c
@@ -1,67 +1,65 @@
-/* 
-   Implements the start_elevator, stop_elevator, issue_request
-   syscalls.
-   Authors: Sai Gunesagaran, Ibrahim Atiya 
-   Team mates: Yilin Wang, Ian Sutton 
-*/
-
 #include "module_data.h"
 
-int start_elevator ()
+/*create elevator*/
+extern struct mutex elevatorLock;
+extern struct mutex floorLock;
+
+int start_elevator()
 {
-	/*create elevator*/
-	elevator_info Elevator; 
 	/*create array of floors*/
-	floor_info *allFloors;
 
 	/*check if elevator is active*/
-	if(continueRun == 1) return(1);
+	if(elevator.continueRun == 1) return(1);
 
 	/*not active*/
 	else{
-	Elevator = malloc(sizeof(elevator_info));
-	/*set elevator status*/
-	mutex_lock(&elevatorLock);
-	Elevator.currentFloor = 1;
-	Elevator.usedSpace = 0;
-	Elevator.usedWeight = 0;
-	Elevator.state = IDLE;
-	Elevator.continueRun = 1;
-	mutex_unlock(&elevatorLock);
-	/*allocate memory for floors*/
-	allFloors = malloc(10*sizeof(floor_info));
-	return (0);}
+		/*set elevator status*/
+		mutex_lock(&elevatorLock);
+		elevator.currentFloor = 1;
+		elevator.usedSpace = 0;
+		elevator.usedWeightUnit = 0;
+		elevator.state = IDLE;
+		elevator.continueRun = 1;
+		elevator.goingDown = 0;
+		mutex_unlock(&elevatorLock);
+		/*allocate memory for floors*/
+		return (0);
+	}
 }
 
 int issue_request(int pass_type, int start_floor, int desired_floor)
 {
 	/*if any of the passed in paramaters are invalid*/
-	if (pass_type >=4 || start_floor <=0 || start_floor >= 11 || desired_floor <= 0 || desired_floor >= 11 || Elevator.continueRun == 0){
+	if (pass_type >=4 || start_floor <=0 || start_floor >= 11 || desired_floor <= 0 || desired_floor >= 11 || elevator.continueRun == 0){
 		return(1);
 	}
-else 
-{
-	passenger_info Passenger = malloc(sizeof(passenger_info)); /*make new passenger*/
-	/*define variables for the passenger*/
-	Passenger.currFloor = start_floor;
-	Passenger.destFloor = desired_floor;
-	Passenger.passengerType = pass_type;
-	/*add passenger to floor*/
-	list_add_tail(*Passenger, allFloors[start_floor].queue);
-	/*increment counter*/
-	mutex_lock(&floorLock);
-	allFloors[start_floor].queueWaiting++;	
-	mutex_unlock(&floorLock);
-	return (0);
-}
 
+	else{
+		/*define variables for the passenger*/
+		passenger.currFloor = start_floor;
+		passenger.destFloor = desired_floor;
+		passenger.passengerType = pass_type;
+		/*check if passenger is going up or down and set bool accordingly*/
+		if (desired_floor - start_floor > 0)
+			passenger.goingUp = 1;
+		else 
+			passenger.goingUp = 0;
+		/*add passenger to floor*/
+		list_add_tail(&passenger.passengerList, &floors[start_floor-1].queue);
+		/*increment counter*/
+		mutex_lock(&floorLock);
+		floors[start_floor].queueWaiting++;	
+		mutex_unlock(&floorLock);
+		return (0);
+	}
+}
 int stop_elevator()
 {
 	/*if there are people on the elevator*/
-	mutex_lock(&elevatorLock)
-	if (Elevator.continueRun)
+	mutex_lock(&elevatorLock);
+	if (elevator.continueRun)
 	{
-		Elevator.continueRun = 0;
+		elevator.continueRun = 0;
 		mutex_unlock(&elevatorLock);	
 		return(0);
 	}
@@ -72,5 +70,3 @@ int stop_elevator()
 
 }
 
-module_data.c
-Displaying module_data.c.
